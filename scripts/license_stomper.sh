dcbdcba6ec2d95166d551beb56ae5ad64d0a3048
kmanc <koins@duck.com>
2025-02-06 10:36:02 -0800

    Create license_stomper.sh

---
+++ scripts/license_stomper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+FILE="/usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js"
+
+# Check if the file exists
+if [ ! -f "$FILE" ]; then
+  echo "Error: File '$FILE' does not exist."
+  exit 1
+fi
+
+# Use sed to replace the line
+sed -i "s/\.data\.status\.toLowerCase() !== 'active')/\.data\.status\.toLowerCase() == 'active')/g" "$FILE"
+
+echo "Replacement completed in file: $FILE"
