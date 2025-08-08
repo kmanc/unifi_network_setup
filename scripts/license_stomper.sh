5abe70a6d7a8b1d2286a1dda0a0989e9d9eb2011
kmanc <koins@duck.com>
2025-08-08 13:59:17 -0400

    Update license_stomper.sh

--- scripts/license_stomper.sh
+++ scripts/license_stomper.sh
@@ -9,6 +9,6 @@ if [ ! -f "$FILE" ]; then
 fi
 
 # Use sed to replace the line
-sed -i "s/\.data\.status\.toLowerCase() !== 'active')/\.data\.status\.toLowerCase() == 'active')/g" "$FILE"
+sed -i "s/\.data\.status\.toLowerCase() !== 'active'/\.data\.status\.toLowerCase() == 'active'/g" "$FILE"
 
 echo "Replacement completed in file: $FILE"
