d2048095583b15c662ec7801839d50589df8e759
kmanc <koins@duck.com>
2026-04-29 20:01:34 -0400

    Update and rename lets_encrypt_certificate_applier.sh to acme_certificate_applier.sh

---
+++ scripts/acme_certificate_applier.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Domain and DNS constants
+DOMAIN="koins"
+SUBDOMAIN="controller"
+TLD="cloud"
+
+# ACME constants
+FULL_CHAIN="/root/.acme.sh/${SUBDOMAIN}.${DOMAIN}.${TLD}_ecc/fullchain.cer"
+PRIV_KEY="/root/.acme.sh/${SUBDOMAIN}.${DOMAIN}.${TLD}_ecc/${SUBDOMAIN}.${DOMAIN}.${TLD}.key"
+
+# Unifi constants
+CRT_FILE="/data/unifi-core/config/unifi-core.crt"
+KEY_FILE="/data/unifi-core/config/unifi-core.key"
+
+/bin/cp "${FULL_CHAIN}" "${CRT_FILE}"
+/bin/cp "${PRIV_KEY}" "${KEY_FILE}"
+
+/bin/chown root:ssl-cert "${CRT_FILE}" "${KEY_FILE}"
+
+/bin/chmod 644 "${CRT_FILE}"
+/bin/chmod 600 "${KEY_FILE}"
